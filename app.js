b5100e8eca6fdd65d304af0db1acba4d236a3cec
Rafał Cisek <rafal.cisek121@gmail.com>
2022-01-23 01:14:38 +0100

    Add Header

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var exphbs = require('express-handlebars').engine;
+var mongoose = require('mongoose');
 
+// connect to db
+mongoose.connect('mongodb://127.0.0.1/checkyns').then(console.log("Connected to databse"));
+
+// routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
